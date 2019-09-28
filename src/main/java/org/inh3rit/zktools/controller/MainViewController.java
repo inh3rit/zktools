f9db2feeddfa9ae3e42afd5ba76f8f527a575268
inh3rit <391552129@qq.com>
2019-09-28 23:46:10 +0800

    update

--- src/main/java/org/inh3rit/zktools/controller/MainViewController.java
+++ src/main/java/org/inh3rit/zktools/controller/MainViewController.java
@@ -3,6 +3,7 @@ package org.inh3rit.zktools.controller;
 import de.felixroske.jfxsupport.FXMLController;
 import javafx.fxml.FXML;
 import javafx.scene.Node;
+import javafx.scene.control.TextField;
 import javafx.scene.control.TreeItem;
 import javafx.scene.control.TreeView;
 import javafx.scene.image.Image;
@@ -27,19 +28,42 @@ public class MainViewController {
     @FXML
     private TreeView rootTree;
 
+    @FXML
+    private TextField urlTxt;
+
     public void initialize() {
     }
 
     @FXML
     private void handleConnect() {
+        String url = urlTxt.getCharacters().toString();
+        if (!checkUrl(url)) {
+            // TODO 提示
+            return;
+        }
+
+
+
+
         Node rootIcon = new ImageView(new Image(getClass().getResourceAsStream("/pics/directory.png")));
         TreeItem<String> rootItem = new TreeItem<>("Zookeeper", rootIcon);
         rootItem.setExpanded(true);
-        for (int i = 1; i < 6; i++) {
-            TreeItem<String> item = new TreeItem<>("Message" + i);
-            rootItem.getChildren().add(item);
-        }
+        TreeItem<String> item1 = new TreeItem<>("Message1");
+        TreeItem<String> item2 = new TreeItem<>("Message1");
+        item1.getChildren().add(item2);
+        rootItem.getChildren().add(item1);
         rootTree.setRoot(rootItem);
+    }
+
+    private boolean checkUrl(String url) {
+        if (!url.contains(":"))
+            return false;
+        String[] configs = url.split(":");
+        if (configs.length != 2)
+            return false;
+        // TODO check ip and port
+
+        return true;
     }
 
 
